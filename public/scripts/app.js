6330651740ebfdfe35f0a6c1a5fd577312c283ea
Brett Miller <miller.brett.andrew@gmail.com>
2017-06-14 14:35:22 -0500

    search and random

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,19 +5,30 @@ var myApp = angular.module('myApp', []);
 myApp.controller('HttpController', function($http) { //need to tell controller using $http
   console.log('NG');
   var vm = this;
-  var searchURL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho';
+
+  var randomURL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=';
 
 
 
   vm.randomGet = function() {
 
     console.log('in randomGet');
-    $http.get(searchURL).then(function(response) {
+    $http.get(randomURL).then(function(response) {
       console.log(response);
-
+      vm.gifs = response.data.data;
 
     });
   }; //end randomGet
+
+  vm.searchGif = function() {
+    var search = vm.search;
+    console.log(search);
+    var searchURL = 'http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=dc6zaTOxFJmzC';
+    $http.get(searchURL).then(function(response) {
+      console.log(response);
+      vm.returnSearch = response.data.data;
+    }); //end searchGif
+  }; //end search function
 
 
 }); //end HttpController
