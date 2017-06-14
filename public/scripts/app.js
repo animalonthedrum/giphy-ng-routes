b26e09528424fa87ce5acb00ec8ab1afb6ba5850
Brett Miller <miller.brett.andrew@gmail.com>
2017-06-14 11:45:28 -0500

    added api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,10 +5,19 @@ var myApp = angular.module('myApp', []);
 myApp.controller('HttpController', function($http) { //need to tell controller using $http
   console.log('NG');
   var vm = this;
+  var searchURL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho';
+
 
 
   vm.randomGet = function() {
+
     console.log('in randomGet');
-  };
+    $http.get(searchURL).then(function(response) {
+      console.log(response);
+
+
+    });
+  }; //end randomGet
+
 
 }); //end HttpController
